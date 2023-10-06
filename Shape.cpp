a0d2ac9e1b748e5a3b5a7257c7c61cc629d32313
Alejandro Rueda Plaza <alejandroruedaplazaarp@gmail.com>
2023-10-06 18:32:18 +0200

    Añadida implementación de la clase Shape; Makefile actualizado

---
+++ Shape.cpp
@@ -0,0 +1,22 @@
+
+#include "Shape.h"
+
+Shape::Shape(){
+	color = "red";
+}
+
+Shape::Shape(std::string c){
+	if(c == "red" || c == "green" || c == "blue")
+		color = c;
+	throw std::invalid_argument("Color no valido");
+}
+
+std::string Shape::get_color(){
+	return color;
+}
+
+void Shape::set_color(std::string c){
+	if(c == "red" || c == "green" || c == "blue")
+		color = c;
+	throw std::invalid_argument("Color no valido");
+}
