49679cabfeddc6df487140d7474f8704d23b1a34
Alejandro Rueda Plaza <alejandroruedaplazaarp@gmail.com>
2023-10-23 21:46:11 +0200

    Añadida implementación de la clase Rectangle; Makefile actualizado;

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,24 +1,30 @@
 #include "Rectangle.h"
 
-//Rectangle::Rectangle(): vs{Point2D(-1,0.5), Point2D(1,0.5), Point2D(1, -0.5), Point2D(-1, -0.5)}{
-	//vs={Point2D(-1,0.5), Point2D(1,0.5), Point2D(1, -0.5), Point2D(-1, -0.5)};
 Rectangle::Rectangle(){
+	vs = new Point2D[N_VERTICES];
 	vs[0] = Point2D(-1,0.5);
 	vs[1] = Point2D(1,0.5);
 	vs[2] = Point2D(1, -0.5);
 	vs[3] = Point2D(-1, -0.5);
+	
 }
 
 
 Rectangle::Rectangle(std::string color, Point2D* vertices){
-	set_color(color);
-	vs = vertices;		
+	if(Point2D::distance(vertices[0], vertices[1]) == Point2D::distance(vertices[2], vertices[3]) && Point2D::distance(vertices[1], vertices[2]) == Point2D::distance(vertices[3], vertices[0]) ){
+		set_color(color);
+		vs = vertices;
+	}else{
+		throw std::invalid_argument("Provided vertices do not build a valid rectangle!");
+	}
+		
 }
 
-Rectangle::Rectangle(const Rectangle &r){
-	for(int i=0; i < N_VERTICES ; i++){
-		vs[i] = r[i];
+Rectangle::Rectangle(const Rectangle &r): vs{new Point2D[4]}{
+	for(int i=0; i< N_VERTICES; i++){
+		vs[i] = r.vs[i];
 	}
+	set_color(r.color);
 }
 
 Rectangle::~Rectangle(){
@@ -29,40 +35,58 @@ Point2D Rectangle::get_vertex(int ind) const{
 	if(ind >= 0 && ind < N_VERTICES){
 		return vs[ind];
 	}
-	throw std::out_of_range("Error get vertex");
+	throw std::out_of_range("ERROR: Fuera de rango [0, N_VERTICES-1]");
 }
 
 Point2D Rectangle::operator[](int ind) const{
+	if(ind < 0 || ind >= N_VERTICES)
+		throw std::out_of_range("ERROR: Fuera de rango [0, N_VERTICES-1]");
 	return get_vertex(ind);
 }
 
-void set_vertices(Point2D* vetices){
-	//TODO
+void Rectangle::set_vertices(Point2D* vertices){	
+	if(Point2D::distance(vertices[0], vertices[1]) == Point2D::distance(vertices[2], vertices[3]) &&
+	 Point2D::distance(vertices[1], vertices[2]) == Point2D::distance(vertices[3], vertices[0]) ){
+		for(int i=0; i < N_VERTICES; i++){
+			vs[0] = vertices[0];
+		}
+	}else{
+		throw std::invalid_argument("Provided vertices do not build a valid rectangle!");
+	}
+
 }
 
 Rectangle& Rectangle::operator=(const Rectangle &r){
-	this->vs = r.vs;
+	for(int i=0; i< N_VERTICES; i++){
+		vs[i] = r.vs[i];
+	}
 	set_color(r.color);
 	return *this;  
+
 }
 
 std::ostream& operator<<(std::ostream &out, const Rectangle &r){
-	out << "HOLA";//TODO
+	out << "[Rectangle: color = " << r.get_color();
+	for(int i = 0; i < r.N_VERTICES; i++)
+		out << "; v" << i << " = " << r.vs[i];
+	out << "]";
+
 	return out;
 }
 
 double Rectangle::area() const{
-	//TODO
-	return 0;
+	return Point2D::distance(vs[0], vs[1]) * Point2D::distance(vs[1], vs[2]);
 }
 
 double Rectangle::perimeter() const{
-	//TODO
-	return 0;
+	return 2 * Point2D::distance(vs[0], vs[1]) + 2 * Point2D::distance(vs[1], vs[2]);
 }
 
-void Rectangle::translate(double incX, double incY){
-	//TODO
+void Rectangle::translate(double incX, double incY){	
+	for(int i=0; i < N_VERTICES; i++){
+		vs[i].x += incX;
+		vs[i].y += incY;
+	}
 }
 
 void Rectangle::print(){
